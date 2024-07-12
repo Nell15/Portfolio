46ed630a94f2fc459118675986915a23fff540f7
Nell15 <nelltruong67@gmail.com>
2024-07-13 00:17:37 +0200

    switched footer to layout + finished pc skills tab + added a few  skills

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -7,38 +7,54 @@
 
 <main>
     <h1>Skills</h1>
-    <p>Here are technologies I am familiar with :</p>
+    <p style="margin-bottom: 2em;">Here are technologies I am either familiar with or learning :</p>
 
     <div class="big-box">
         <div class="tab-buttons">
             <button on:click={() => {
                 use = "all"
-            }}>
+            }} class:open={use === "all"}>
                 All
             </button>
             <button on:click={() => {
                 use = "soft"
-            }}>
+            }} class:open={use === "soft"}>
                 Software Development
             </button>
             <button on:click={() => {
                 use = "web"
-            }}>
+            }} class:open={use === "web"}>
                 Web Development
             </button>
+            <button on:click={() => {
+                use = "game"
+            }} class:open={use === "game"}>
+                Game Development
+            </button>
+            <button on:click={() => {
+                use = "ia"
+            }} class:open={use === "ia"}>
+                Machine Learning
+            </button>
+            <button on:click={() => {
+                use = "software"
+            }} class:open={use === "software"}>
+                Softwares
+            </button>
         </div>
-        
-        <div class='lang_container'>
-            {#each langs as l}
-                {#if l.use.includes(use)}
-                    <div class='lang'>
-                        <div class='img'>
-                            <img src={`${path}${l.file}`} alt={l.name} class='lang_img'>
+        <div class="tab">
+            <div class='lang_container'>
+                {#each langs as l}
+                    {#if l.use.includes(use)}
+                        <div class='lang'>
+                            <div class='img'>
+                                <img src={`${path}${l.file}`} alt={l.name} class='lang_img'>
+                            </div>
+                            <p style="text-align:center">{l.name}</p>
                         </div>
-                        <p style="text-align:center">{l.name}</p>
-                    </div>
-                {/if}
-            {/each}
+                    {/if}
+                {/each}
+            </div>
         </div>
     </div>
 </main>
@@ -48,14 +64,15 @@
     main {
         display: flex;
         flex-direction: column;
-        justify-content: center;
         align-items: center;
+        min-height: 100vh;
     }
 
     .big-box{
         display: flex;
         flex-direction: column;
         justify-content: start;
+        filter: drop-shadow(0 0 5px rgba(0, 0, 0, 0.6));
     }
 
     .tab-buttons {
@@ -71,29 +88,43 @@
         border-radius: 1.25em 1.25em 0 0;
         background-color: #e9e9ed;
         transition: all 0.2s ease-in-out;
+        cursor: pointer;
     }
     
     .tab-buttons button:hover {
         background-color: #c8c8cd;
+        cursor: pointer;
+    }
+    
+    .tab-buttons button.open {
+        transition: all 0.2s ease-in-out;
+        background-color: #fafbfe;
+        cursor: default;
+    }
+
+    .tab {
+        background-color: #fafbfe;
+        border-radius: 0 0 2em 2em;
+        display: flex;
+        justify-content: center;
     }
 
     .lang_container {
-        width: 30rem;
+        width: 50rem;
         display: flex;
         flex-wrap: wrap;
         justify-content: start;
-        padding-top: 2rem;
-        border: solid;
-        border-width: 2px;
-        border-color: #e9e9ed;
-        border-radius: 0 2em 2em 2em;
+        padding: 2rem 2rem 0 2rem;
+        margin-bottom: 1.5rem;
     }
 
     .lang {
         height: 8rem;
         display: flex;
+        justify-content: center;
         flex-direction: column;
-        padding: 0px 1em
+        padding: 0px 1em;
+        align-items: center;
     }
 
     .lang_img {
@@ -101,14 +132,20 @@
         width: 4rem;
         object-fit: contain;
     }
-
+    
     .img {
         height: 4rem;
+        width: 4rem;
         transition: all 0.3s ease;
     }
 
     .img:hover {
-        /* height: 4rem; */
         transform: scale(1.2);
+    }
+
+    .lang > p {
+        width: 5rem;
+        margin-top: 1em;
+        margin-bottom: 0;
     }
 </style>
