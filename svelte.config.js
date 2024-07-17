ea28b2a69827ccef0fb57f3c09625b3302bde501
Nell15 <nelltruong67@gmail.com>
2024-07-17 21:54:18 +0200

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,34 +1,15 @@
-// import adapter from '@sveltejs/adapter-auto';
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-// 		adapter: adapter(),
-		
-// 		alias: {
-// 			$assets: "src/assets"
-// 		},
-// 	}
-// };
-
-// export default config;
-
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		
 		alias: {
-		$assets: "src/assets"
+			$assets: "src/assets"
 		},
 	}
 };
