c9601b5e7e9c3997ec18b61adaf71edb3acb76a8
Nell15 <nelltruong67@gmail.com>
2024-07-17 20:29:31 +0200

    im praying rn

--- svelte.config.js
+++ svelte.config.js
@@ -8,12 +8,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		prerender: {
-			handleMissingId: 'ignore'
-		},
 		paths: {
-			// base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-			base: process.argv.includes('dev') ? '' : "/Portfolio"
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 		},
 
 		alias: {
